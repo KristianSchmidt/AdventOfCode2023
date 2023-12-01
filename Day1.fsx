6cefa5ef2982e8b15e8baafa740431e69f877e49
Kristian Schmidt <kristian@kreutz.us>
2023-12-01 18:58:53 +0100

    Day 1 part 1

--- Day1.fsx
+++ Day1.fsx
@@ -4,9 +4,18 @@ open System
 
 Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
-let data = Helpers.Web.getInput 1
+let isDigit c =
+    c = '0' || c = '1' || c = '2' ||
+    c = '3' || c = '4' || c = '5' ||
+    c = '6' || c = '7' || c = '8' ||
+    c = '9'
 
-let ans1 = data
+let data = Helpers.Web.getInput 1
+    
+let ans1 =
+    data
+    |> Array.map (fun s -> s.ToCharArray() |> Array.filter isDigit |> Array.map (string>>int))
+    |> Array.sumBy (fun arr -> arr[0]*10 + arr[arr.Length-1])
 
 ans1
 
