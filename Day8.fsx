35258fe01784d9472f6771c6b29e63bf888ad742
Kristian Bjerre Schmidt <kristian@kreutz.us>
2023-12-11 08:50:37 +0100

    Day 8 solution

--- Day8.fsx
+++ Day8.fsx
@@ -6,12 +6,35 @@ Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
 let data = Helpers.Web.getInput 8
 
-let ans1 = data
+let dir = data[0].ToCharArray()
+
+let map =
+    data[2..]
+    |> Array.map (function Helpers.Regex "(\w\w\w) = \((\w\w\w), (\w\w\w)\)" [s;l;r] -> (s,Map [('L',l); ('R',r)] ))
+    |> Map.ofArray
+
+let findSteps endCondition source =
+    let rec f s (i : int) =
+        match s with
+        | s' when (endCondition s') -> i
+        | s' -> f map.[s'].[dir[i % dir.Length]] (i+1)
+    f source 0
+
+let ans1 = findSteps ((=)"ZZZ") "AAA"
 
 ans1
 
 /// Part 2
 
-let ans2 = data
+let sources =
+    map
+    |> Map.filter (fun k v -> k.EndsWith("A"))
+    |> Map.keys
+    |> Array.ofSeq
+
+let ans2 =
+    sources
+    |> Array.map (findSteps (fun s -> s.EndsWith('Z')) >> int64)
+    |> Array.reduce Helpers.lcm
 
 ans2
