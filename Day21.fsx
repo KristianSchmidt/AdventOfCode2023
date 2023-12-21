f0fe0ac34a0a86468dff4ebdcb8fdb5a89c41d0b
Kristian Schmidt <kristian@kreutz.us>
2023-12-21 08:52:53 +0100

    Day 21 part 1

--- Day21.fsx
+++ Day21.fsx
@@ -4,9 +4,33 @@ open System
 
 Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
-let data = Helpers.Web.getInput 21
+let data = Helpers.Web.getInput 21 |> Array.map (fun s -> s.ToCharArray())
 
-let ans1 = data
+let maxR, maxC = data.Length-1, data[0].Length-1
+
+let isValid (r,c) = r >= 0 && r <= maxR && c >= 0 && c <= maxC
+
+let getNeighbors (r,c) =
+    [| (r+1,c); (r-1,c); (r,c+1); (r,c-1) |]
+    |> Array.filter isValid
+    |> Array.filter (fun (r,c) -> data[r % maxR][c % maxC] <> '#')
+
+let start =
+    let row = data |> Array.findIndex (fun arr -> arr |> Array.contains 'S')
+    let col = data[row] |> Array.findIndex ((=)'S')
+    row, col
+
+let simulate start endStep =
+    let rec f current step =
+        if step = endStep then
+            current
+        else
+            let next = current |> Array.collect getNeighbors |> Array.distinct
+            f next (step + 1)
+
+    f [|start|] 0
+
+let ans1 = simulate start 64 |> Array.length
 
 ans1
 
